67b2ec09f39e28036888f730a86818a7042a5119
赵阳阳 <imqingyue@163.com>
2024-12-30 14:13:59 +0800

    Site updated: 2024-12-30 14:13:59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735529965473";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735539234956";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
