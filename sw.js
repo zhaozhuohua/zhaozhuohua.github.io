6d1fe07a203e8401bd63e3e4f2d5502e9c29fdf5
赵阳阳 <imqingyue@163.com>
2024-12-30 17:08:02 +0800

    Site updated: 2024-12-30 17:08:02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735549497830";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735549681631";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
